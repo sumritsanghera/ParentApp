715cc2606e8120b39fbb22e0edf75bbc4f48a995
hanin <hanin@sfu.ca>
2021-11-04 14:37:34 -0700

    Completed a design of the main menu.

---
+++ app/src/main/java/cmpt276/groupassignment/parentapp/Main_menu.java
@@ -0,0 +1,29 @@
+package cmpt276.groupassignment.parentapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Main_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        setup_coin_flip();
+    }
+
+    private void setup_coin_flip() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
