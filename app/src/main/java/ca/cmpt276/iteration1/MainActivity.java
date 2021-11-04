e17eefc1ac25a37e986ca246254f71e67afdbe86
Md Yousuf SHekh <ysyousuf47@gmail.com>
2021-11-03 20:16:44 -0700

    Add name button is now functional. and edit name button is to some extent functional

--- app/src/main/java/ca/cmpt276/iteration1/MainActivity.java
+++ app/src/main/java/ca/cmpt276/iteration1/MainActivity.java
@@ -9,12 +9,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_FOR_NAME = 101;
 
+    List<String > StrArr=new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +35,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_FOR_NAME);
             }
         });
+
+        Button edit_name=(Button) findViewById(R.id.editName);
+        edit_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                Intent intent=MainActivity3.makeIntent(MainActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_FOR_NAME);
+            }
+        });
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         switch(requestCode){
             case REQUEST_CODE_FOR_NAME:
                 if(resultCode== Activity.RESULT_OK){
-                    String str=data.getStringExtra("newName");
+                    String str=data.getStringExtra("NewName");
+                    StrArr.add(str);
                     Log.i("MyApp", "Result is : " +  str);
+                    Toast.makeText(MainActivity.this, "added name is " + str, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "added name is " + StrArr.toString(), Toast.LENGTH_SHORT).show();
+
                 }
                 else{
                     Log.i("MyApp", "No result found");
