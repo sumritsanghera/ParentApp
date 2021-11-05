f227cd39432cb49830927646dbc95c9121ef1502
hanin <hanin@sfu.ca>
2021-11-04 18:14:29 -0700

    Add set up launcher for coin flip button

--- app/src/main/java/ca/cmpt276/iteration1/Main_menu.java
+++ app/src/main/java/ca/cmpt276/iteration1/Main_menu.java
@@ -1,11 +1,18 @@
 package ca.cmpt276.iteration1;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import cmpt276.groupassignment.parentapp.model.Coin_Flip;
+import cmpt276.groupassignment.parentapp.model.Coin_Flip_Manager;
 
 /*
        Main Menu class using layout: activity_main_menu
@@ -18,17 +25,43 @@ import android.view.MenuItem;
  */
 
 public class Main_menu extends AppCompatActivity {
+    private Coin_Flip_Manager manager;
+    private ActivityResultLauncher<Intent> coin_flip_launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        setup_launcher();
         setup_coin_flip();
+        setup_history_button();
+    }
+
+    private void setup_launcher() {
+        coin_flip_launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        assert data != null;
+
+                        String picker = data.getStringExtra("PICKER");
+                        boolean returned_result = data.getBooleanExtra("RESULT",false);
+                        String time = data.getStringExtra("TIME");
+
+                        Coin_Flip new_flip = new Coin_Flip(picker,returned_result,time);
+                        manager.add_flip(new_flip);
+                    }
+                });
     }
 
     private void setup_coin_flip() {
 
+    }
+
+    private void setup_history_button() {
     }
 
     @Override
