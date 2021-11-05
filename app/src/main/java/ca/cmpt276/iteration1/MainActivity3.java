d15db999f16825f3145bcd848d7b8065ee6d0079
Md Yousuf SHekh <ysyousuf47@gmail.com>
2021-11-04 23:19:52 -0700

    Add name button is now functional. and edit name button is to some extent functional

--- app/src/main/java/ca/cmpt276/iteration1/MainActivity3.java
+++ app/src/main/java/ca/cmpt276/iteration1/MainActivity3.java
@@ -1,12 +1,19 @@
 package ca.cmpt276.iteration1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity3 extends AppCompatActivity {
 
